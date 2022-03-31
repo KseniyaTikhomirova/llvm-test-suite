02ddd1f956dd0a64741b2b7c4a042fbb2574b391
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2022-03-31 10:18:05 +0300

    Change run command format
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- SYCL/KernelAndProgram/level-zero-static-link-flow.cpp
+++ SYCL/KernelAndProgram/level-zero-static-link-flow.cpp
@@ -1,7 +1,6 @@
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 ZE_DEBUG=1 %t.out 2>&1 | FileCheck %s
-// RUN: %GPU_RUN_PLACEHOLDER SYCL_PI_TRACE=-1 ZE_DEBUG=1 \
-// RUN: CreateMultipleRootDevices=2 NEOReadDebugKeys=1 %t.out 2>&1 | FileCheck %s
+// RUN: env SYCL_PI_TRACE=-1 ZE_DEBUG=1 CreateMultipleRootDevices=2 NEOReadDebugKeys=1 %GPU_RUN_PLACEHOLDER %t.out 2>&1 | FileCheck %s
 // REQUIRES: level_zero
 //
 //==--- level-zero-static-link-flow.cpp.cpp - Check L0 static link flow --==//
