5c7aaf9be268fa80c9b4c5bd5dc1b3554d14f632
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2022-04-01 22:16:50 +0300

    Check if env really works
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- SYCL/Basic/query_emulate_subdevice.cpp
+++ SYCL/Basic/query_emulate_subdevice.cpp
@@ -1,5 +1,5 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
-// RUN: env CreateMultipleSubDevices=2 EnableTimestampPacket=1 \
+// RUN: env CreateMultipleSubDevices=1 EnableTimestampPacket=1 \
 // RUN: NEOReadDebugKeys=1 SYCL_DEVICE_FILTER="gpu" %t.out
 
 // UNSUPPORTED: gpu-intel-dg1,cuda,hip
