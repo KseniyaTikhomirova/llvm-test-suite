da88b6938901e8e44e4d26364ea4d856d9fe449b
Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>
2022-09-08 04:54:10 -0700

    Remove not relevant XFAIL from AtomicRef for HIP
    Signed-off-by: Tikhomirova, Kseniya <kseniya.tikhomirova@intel.com>

--- SYCL/AtomicRef/device_has_aspect_atomic64_cuda_and_hip.cpp
+++ SYCL/AtomicRef/device_has_aspect_atomic64_cuda_and_hip.cpp
@@ -1,11 +1,6 @@
-// FIXME unexpected pass on hip
-// UNSUPPORTED: hip
 // REQUIRES: cuda || hip
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
-
-// XFAIL: hip
-// Expected failure because hip does not have atomic64 check implementation
 
 #include <CL/sycl.hpp>
 #include <iostream>
